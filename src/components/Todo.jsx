f0c3b7ca2f2d96b244cc3d337ed568f2f1b06f80
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-03-19 17:43:13 +0530

    Bug Fix : UI

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,13 +6,24 @@ const Todo = ({todo, handleDelete, handleStatusChange}) => {
         <div className="px-2">
             <div className='card mt-2 p-2'>
                  
-                <div className="card-title d-flex justify-content-between">
-                    <div className="fw-semibold">
+                <div className="card-title d-flex justify-content-between mb-0">
+                    <span className="fw-normal fs-6">
                         Created At:
-                    </div>
-                    <div>{new Date(todo.id).toLocaleString()}</div>
+                    </span>
+                    <span className="fw-normal fs-6">{new Date(todo.id).toLocaleString()}</span>
                     
                 </div>
+
+
+                <hr />
+
+                <div className='card-body p-2'>
+
+
+                <p style={{textDecoration: todo.status === "Completed" ? "line-through":"none"}}>{todo.name}</p>
+
+                </div>
+
 
                 <div className="d-flex justify-content-between mt-3">
                 <select id="" className="btn btn-sm btn-outline-success" onChange={(e) => handleStatusChange(e, todo.id)}>
@@ -24,16 +35,6 @@ const Todo = ({todo, handleDelete, handleStatusChange}) => {
 
                 
                 <button className='btn btn-sm btn-danger' onClick={() => handleDelete(todo.id)}>Delete</button>
-                </div>
-
-
-                <hr />
-
-                <div className='card-body p-2'>
-
-
-                <p style={{textDecoration: todo.status === "Completed" ? "line-through":"none"}}>{todo.name}</p>
-
                 </div>
             </div>
         </div>
